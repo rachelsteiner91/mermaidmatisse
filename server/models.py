4548de196554cfc5d71cf2ac9ddcf9be7d64bd2b
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-07-05 17:20:52 -0400

    seed columns updated image to artworks

--- server/models.py
+++ server/models.py
@@ -15,8 +15,8 @@ class User(db.Model, SerializerMixin):
     role = db.Column(db.String)
     created_at = db.Column(db.DateTime)
 ## USER RELATIONSHIPS
-    collections = db.relationship('Collection', back_populates='user.id')
-    art_collection = db.relationship('ArtCollection', back_populates='user.id')
+    collections = db.relationship('Collection', back_populates='user')
+   
 ##VALIDATIONS
 
 # #~~~~~~~~~~~~~~~# 
@@ -27,9 +27,11 @@ class Artwork(db.Model, SerializerMixin):
     artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
     title = db.Column(db.String)
     medium = db.Column(db.String)
+    image = db.Column(db.String)
     created_at = db.Column(db.DateTime)
 ##RELATIONSHIPS    
-
+    artist = db.relationship('Artist', back_populates='artworks')
+    collections = db.relationship('Collection', back_populates='artwork')
 ##VALIDATIONS
 # #~~~~~~~~~~~~~~~# 
 class Collection(db.Model, SerializerMixin):
@@ -50,10 +52,73 @@ class Artist(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     medium = db.Column(db.String)
-    artworks = db.relationship('Artwork', back_populates='artists')
+    artworks = db.relationship('Artwork', back_populates='artist')
 
 # class ArtCollection(db.Model, SerializerMixin):
 #     __tablename__="art_collections"
 
 #     artwork_id = db.Column(db.Integer, db.ForeignKey('artworks.id'), primary_key=True)
 #     collection_id = db.Column(db.Integer, db.ForeignKey('collections.id'), primary_key=True)
+
+
+# from sqlalchemy_serializer import SerializerMixin
+# from config import db
+
+# # Models go here!
+
+# ##USER
+# class User(db.Model, SerializerMixin):
+#     __tablename__ = "users"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     username = db.Column(db.String, nullable=False)
+#     role = db.Column(db.String)
+#     created_at = db.Column(db.DateTime)
+    
+#     ## USER RELATIONSHIPS
+#     collections = db.relationship('Collection', back_populates='user')
+#     art_collection = db.relationship('ArtCollection', back_populates='user')
+
+# ##VALIDATIONS
+
+# # #~~~~~~~~~~~~~~~# 
+# class Artwork(db.Model, SerializerMixin):
+#     __tablename__ = "artworks"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+#     title = db.Column(db.String)
+#     medium = db.Column(db.String)
+#     image = db.Column(db.String)
+#     created_at = db.Column(db.DateTime)
+    
+#     ##RELATIONSHIPS    
+#     artist = db.relationship('Artist', back_populates='artworks')
+#     collections = db.relationship('Collection', back_populates='artwork')
+
+# ##VALIDATIONS
+
+# # #~~~~~~~~~~~~~~~# 
+# class Collection(db.Model, SerializerMixin):
+#     __tablename__ = "collections"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     artwork_id = db.Column(db.Integer, db.ForeignKey('artworks.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     created_at = db.Column(db.DateTime)
+    
+#     ##RELATIONSHIPS    
+#     artwork = db.relationship('Artwork', back_populates='collections')  
+
+# ##VALIDATIONS
+
+# # #~~~~~~~~~~~~~~~# 
+# class Artist(db.Model, SerializerMixin):
+#     __tablename__ = "artists"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     medium = db.Column(db.String)
+    
+#     artworks = db.relationship('Artwork', back_populates='artist')
--- server/seed.py
+++ server/seed.py
@@ -15,3 +15,16 @@ if __name__ == '__main__':
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+        # print("Seeding artists...")
+        # artists = [
+        #     Artist(name="Jessie Makinson", medium="painting")
+        # ]
+        # db.session.add_all(artists)
+
+        # print("Seeding artworks...")
+        # artworks = [
+        #     Artwork(title="we shall be monsters", artist_id=1, medium="painting", image="https://cdn.discordapp.com/attachments/1113536081091108996/1126256462751088640/IMG_5E8598DF9AAC-1.jpeg")
+        # ]
+
+        # db.session.add_all(artworks)
