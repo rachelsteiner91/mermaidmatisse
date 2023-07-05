c5bb54828fbd5615d04d70f1a0946c0f0fe5616e
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-07-05 11:33:27 -0400

    table work

--- server/app.py
+++ server/app.py
@@ -8,9 +8,13 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+# from models import Curator
 
 # Views go here!
+@app.route("/")
+# @cross_origin()
+def howdy():
+  return "Howdy partner!"
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,3 +3,15 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+
+#Curator
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False, unique=True)
+
+##RELATIONSHIPS
+
+##VALIDATIONS
