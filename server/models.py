5982beb729bf4c9f9ba7e4198865b43ae6f40cd2
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-07-06 10:32:03 -0400

    seed populated

--- server/models.py
+++ server/models.py
@@ -3,6 +3,13 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db
 
+
+#~~~~~~~~~~~~~~~#
+from sqlalchemy_serializer import SerializerMixin
+
+
+from config import db
+
 # Models go here!
 
 ##USER
@@ -15,8 +22,8 @@ class User(db.Model, SerializerMixin):
     role = db.Column(db.String)
     created_at = db.Column(db.DateTime)
 ## USER RELATIONSHIPS
-    collections = db.relationship('Collection', back_populates='user')
-   
+    collections = db.relationship('Collection', back_populates='artworks')
+    # art_collection = db.relationship('ArtCollection', back_populates='user.id')
 ##VALIDATIONS
 
 # #~~~~~~~~~~~~~~~# 
@@ -30,8 +37,7 @@ class Artwork(db.Model, SerializerMixin):
     image = db.Column(db.String)
     created_at = db.Column(db.DateTime)
 ##RELATIONSHIPS    
-    artist = db.relationship('Artist', back_populates='artworks')
-    collections = db.relationship('Collection', back_populates='artwork')
+    artists = db.relationship('Artist', back_populates ='artworks')
 ##VALIDATIONS
 # #~~~~~~~~~~~~~~~# 
 class Collection(db.Model, SerializerMixin):
@@ -42,7 +48,7 @@ class Collection(db.Model, SerializerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     created_at = db.Column(db.DateTime)
 ##RELATIONSHIPS    
-    artworks = db.relationship('Artwork', back_populates='collections')  
+    artworks = db.relationship('User', back_populates='collections')  
 ##VALIDATIONS
 
 # #~~~~~~~~~~~~~~~# 
@@ -52,7 +58,61 @@ class Artist(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     medium = db.Column(db.String)
-    artworks = db.relationship('Artwork', back_populates='artist')
+    artworks = db.relationship('Artwork', back_populates='artists')
+
+
+
+
+    # Models go here!
+
+##USER
+# class User(db.Model, SerializerMixin):
+#     __tablename__ = "users"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     username = db.Column(db.String, nullable=False)
+#     role = db.Column(db.String)
+#     created_at = db.Column(db.DateTime)
+# ## USER RELATIONSHIPS
+#     collections = db.relationship('Collection', back_populates='user')
+   
+# ##VALIDATIONS
+
+# # #~~~~~~~~~~~~~~~# 
+# class Artwork(db.Model, SerializerMixin):
+#     __tablename__ = "artworks"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+#     title = db.Column(db.String)
+#     medium = db.Column(db.String)
+#     image = db.Column(db.String)
+#     created_at = db.Column(db.DateTime)
+# ##RELATIONSHIPS    
+#     artist = db.relationship('Artist', back_populates='artworks')
+#     collections = db.relationship('Collection', back_populates='artwork')
+# ##VALIDATIONS
+# # #~~~~~~~~~~~~~~~# 
+# class Collection(db.Model, SerializerMixin):
+#     __tablename__ = "collections"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     artwork_id = db.Column(db.Integer, db.ForeignKey('artworks.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     created_at = db.Column(db.DateTime)
+# ##RELATIONSHIPS    
+#     artworks = db.relationship('Artwork', back_populates='collections')  
+# ##VALIDATIONS
+
+# # #~~~~~~~~~~~~~~~# 
+# class Artist(db.Model, SerializerMixin):
+#     __tablename__ = "artists"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     medium = db.Column(db.String)
+#     artworks = db.relationship('Artwork', back_populates='artist')
 
 # class ArtCollection(db.Model, SerializerMixin):
 #     __tablename__="art_collections"
@@ -122,3 +182,6 @@ class Artist(db.Model, SerializerMixin):
 #     medium = db.Column(db.String)
     
 #     artworks = db.relationship('Artwork', back_populates='artist')
+
+
+
--- server/seed.py
+++ server/seed.py
@@ -8,12 +8,44 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Artist, Artwork
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
+
+
+        print("Seeding artists...")
+        artists = [
+            Artist(name="Jessie Makinson", medium="painting")
+        ]
+        db.session.add_all(artists)
+        db.session.commit()
+
+        print("Seeding users...")
+        users = [
+            User(name="John Doe", username="johndoe", role="admin"),
+            User(name="Jane Smith", username="janesmith", role="user")
+        ]
+        db.session.add_all(users)
+        db.session.commit()
+
+        print("Seeding artworks...")
+        artworks = [
+            Artwork(title="we shall be monsters", artist_id=1, medium="painting", image="https://cdn.discordapp.com/attachments/1113536081091108996/1126256462751088640/IMG_5E8598DF9AAC-1.jpeg")
+        ]
+        db.session.add_all(artworks)
+        db.session.commit()
+
+        # print("Seeding collections...")
+        # collections = [
+        #     Collection(user_id=1, artwork_id=1)
+        # ]
+        # db.session.add_all(collections)
+        # db.session.commit()
+
+        # print("Seed completed successfully!")
         # Seed code goes here!
 
         # print("Seeding artists...")
@@ -21,6 +53,7 @@ if __name__ == '__main__':
         #     Artist(name="Jessie Makinson", medium="painting")
         # ]
         # db.session.add_all(artists)
+        # db.session.commit()
 
         # print("Seeding artworks...")
         # artworks = [
@@ -28,3 +61,6 @@ if __name__ == '__main__':
         # ]
 
         # db.session.add_all(artworks)
+        # db.session.commit()
+
+        print("Seed completed successfully!")
