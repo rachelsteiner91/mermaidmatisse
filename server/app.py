334ddb0552f4829b7d5b1a725e9b9814580d5ac7
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-07-07 12:03:05 -0400

    api routes

--- server/app.py
+++ server/app.py
@@ -19,6 +19,8 @@ migrate = Migrate(app, db)
 def howdy():
   return "Howdy partner!"
 
+#~~~~~~~Artworks~~~~~~~~~#
+
 # GET/artworks -- need to add ##only to get
 class Artworks(Resource):
    def get(self):
@@ -30,6 +32,78 @@ class Artworks(Resource):
       )
       return res
 api.add_resource(Artworks, '/artworks')
+#GET/ artworks/<int:id>
+class OneArtwork(Resource):
+   def get(self, id):
+        artwork = Artwork.query.filter_by(id=id).first()
+        if not artwork:
+            return {"404": "Artwork Not Found"}, 404
+        
+        res = make_response(
+            artwork.to_dict(only=('id', 'title', 'medium', 'image', 'artist_id')),
+            200
+        )
+        return res
+api.add_resource(OneArtwork, "/artworks/<int:id>")
+#~~~~~~~Styles~~~~~~~~~#
+
+#GET / styles
+class Styles(Resource):
+   def get(self):
+      styles = Style.query.all()
+      styles_dict = [s.to_dict() for s in styles]
+      res = make_response(
+         styles_dict,
+         200
+      )
+      return res
+api.add_resource(Styles, '/styles')
+#GET /styles/<int:id>
+class OneStyle(Resource):
+   def get(self, id):
+        style = Style.query.filter_by(id=id).first()
+        if not style:
+            return {"404": "Style Not Found"}, 404
+        
+        res = make_response(
+            style.to_dict(only=('id', 'style_type')),
+            200
+        )
+        return res
+api.add_resource(OneStyle, "/styles/<int:id>")
+
+
+
+
+
+#~~~~~~~~Artists~~~~~~~~#
+
+
+
+
+
+#~~~~~~~Collections~~~~~~~~~#
+
+
+
+
+
+
+#~~~~~~~Users~~~~~~~~~#
+class Users(Resource):
+   def get(self):
+      users = User.query.all()
+      users_dict = [u.to_dict() for u in users]
+      res = make_response(
+         users_dict,
+         200
+      )
+      return res
+   
+api.add_resource(Users, '/users')
+
+
+
    
 
 
