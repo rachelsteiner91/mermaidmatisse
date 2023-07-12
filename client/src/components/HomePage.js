f21b2737f8008483b8107d35ac949cb1737f5018
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-07-12 14:33:57 -0400

    artwork title and artist name added below carousel success

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -19,9 +19,12 @@ function HomePage({artworks}){
              <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
              <img src={artwork.image} className="carousel-image" alt={`Artwork ${index + 1}`} style={{ maxWidth: '500px', maxHeight: '400px' }} />
              <section>
-             <p>{artwork.artists.name}</p>
-              <p>{artwork.title}</p>
-             </section>
+             <div className="carousel-text">
+            <p style={{ marginTop: '10px', fontSize: '12px' }}>{artwork.artists.name}, {artwork.title}</p> 
+            <p style={{ marginTop: '30px' }}></p>
+           
+          </div>
+             </section> 
            </div>
          ))}
           </Carousel>
