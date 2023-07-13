35bf6dc38e632936f1938d752aa0ef039b6b93e2
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-07-13 12:55:01 -0400

    artdetail modal zoom in on image wow that was rough and css for zoom in buttons too

--- client/src/components/ArtDetail.js
+++ client/src/components/ArtDetail.js
@@ -1,5 +1,78 @@
+// import { useParams } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import Modal from "react-modal";
+
+// function ArtDetail() {
+//   const [artwork, setArtwork] = useState({
+//     roles: [],
+//     artists: {},
+//     style: {}
+//   });
+//   const [error, setError] = useState(null);
+//   const [isModalOpen, setIsModalOpen] = useState(false); // Track modal state
+
+//   const params = useParams();
+
+//   useEffect(() => {
+//     fetch(`http://localhost:5555/artworks/${params.id}`)
+//       .then((res) => {
+//         if (res.ok) {
+//           return res.json();
+//         } else {
+//           throw new Error("Failed to fetch artwork");
+//         }
+//       })
+//       .then((data) => setArtwork(data))
+//       .catch((error) => setError(error.message));
+//   }, [params.id]);
+
+//   const { id, title, image, artists, medium, style } = artwork;
+
+//   if (error) return <h2>{error}</h2>;
+
+//   const handleImageClick = () => {
+//     setIsModalOpen(true); // Open the modal when the image is clicked
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false); // Close the modal
+//   };
+
+//   return (
+//     <div className="art-detail" id={id}>
+//       <h1>{title}</h1>
+//       <p>{artists.name}</p>
+
+//       <div className="project-card">
+//         <figure className="image">
+//           <img
+//             src={image}
+//             alt={title}
+//             style={{ maxWidth: "400px", maxHeight: "400px", cursor: "zoom-in" }}
+//             onClick={handleImageClick} // Call handleImageClick on image click
+//           />
+//           <section>
+//             <p>Style: {style.style_type}</p>
+//             <p>Artist: {artists.name}</p>
+//             <p>Medium: {medium}</p>
+//           </section>
+//         </figure>
+//         <section className="details"></section>
+//       </div>
+
+//       {/* Modal component from your library */}
+//       <Modal isOpen={isModalOpen} onRequestClose={closeModal} ariaHideApp={false}>
+//         <button onClick={closeModal} className="close-button">Close</button>
+//         <img src={image} alt={title} style={{ maxHeight: "100%" }} />
+//       </Modal>
+//     </div>
+//   );
+// }
+
+// export default ArtDetail;
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Modal from "react-modal";
 
 function ArtDetail() {
   const [artwork, setArtwork] = useState({
@@ -8,6 +81,8 @@ function ArtDetail() {
     style: {}
   });
   const [error, setError] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [zoomLevel, setZoomLevel] = useState(1); 
 
   const params = useParams();
 
@@ -24,11 +99,26 @@ function ArtDetail() {
       .catch((error) => setError(error.message));
   }, [params.id]);
 
-  
-
   const { id, title, image, artists, medium, style } = artwork;
 
   if (error) return <h2>{error}</h2>;
+
+  const handleImageClick = () => {
+    setIsModalOpen(true); 
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false); 
+    setZoomLevel(1); 
+  };
+
+  const handleZoomIn = () => {
+    setZoomLevel(prevZoomLevel => prevZoomLevel + 0.1); 
+  };
+
+  const handleZoomOut = () => {
+    setZoomLevel(prevZoomLevel => Math.max(prevZoomLevel - 0.1, 1));
+  };
 
   return (
     <div className="art-detail" id={id}>
@@ -37,20 +127,43 @@ function ArtDetail() {
 
       <div className="project-card">
         <figure className="image">
-        <img src={image} alt={title} style={{ maxWidth: "400px", maxHeight: "400px" }} />
+          <img
+            src={image}
+            alt={title}
+            style={{ maxWidth: "400px", maxHeight: "400px", cursor: "zoom-in" }}
+            onClick={handleImageClick} 
+          />
           <section>
             <p>Style: {style.style_type}</p>
             <p>Artist: {artists.name}</p>
             <p>Medium: {medium}</p>
           </section>
         </figure>
-        <section className="details">
-          
-        </section>
+        <section className="details"></section>
       </div>
+
+  
+      <Modal isOpen={isModalOpen} onRequestClose={closeModal} ariaHideApp={false}>
+        <button onClick={closeModal} className="close-button">Close</button>
+        <div className="modal-image-container">
+          <img
+            src={image}
+            alt={title}
+            style={{ transform: `scale(${zoomLevel})` }} 
+          />
+          <div className="zoom-controls">
+            <button onClick={handleZoomIn} className="zoom-button">+</button>
+            <button onClick={handleZoomOut} className="zoom-button">-</button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
 
 export default ArtDetail;
+
+
+
+
 
